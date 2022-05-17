e8db7ab8cdb9e2dcd58a5ed6291bc9fd87cf19cc
James Woodward <jamesrobertwoodward@gmail.com>
2022-05-17 10:27:29 -0500

    add createPost method

--- src/main/java/com/example/restblog/web/PostsController.java
+++ src/main/java/com/example/restblog/web/PostsController.java
@@ -1,13 +1,12 @@
 package com.example.restblog.web;
 
 import com.example.restblog.data.Post;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+
 
 @RestController
 @RequestMapping(value="/api/posts", headers = "Accept=application/json")
@@ -28,14 +27,14 @@ public class PostsController {
 
     @GetMapping("/{id}")
     public Post getById(@RequestParam("id") Long id) {
-        List<Post> postList = new ArrayList<>();
-        Post postOne = new Post(1L,"Cheese is great","Most people love cheese");
-        Post postTwo = new Post(2L,"What is sleep?", "Sleep is the cousin of death");
-        Post postThree = new Post(3L,"Javelin vs. T-72BV", "Javelin wins");
-        postList.add(postOne);
-        postList.add(postTwo);
-        postList.add(postThree);
+        Post testPost = new Post(4L, "What IS a platypus?", "A platypus is a weird venomous duck beaver.");
 
-        return postList.get(id.intValue() - 1);
+        return testPost;
     }
+
+    @PostMapping
+    public void createPost(@RequestBody Post newPost) {
+        System.out.println(newPost);
+    }
+
 }

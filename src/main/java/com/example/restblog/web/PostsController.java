970299beaed7197d86e6add5f4cb73147bc7aa88
James Woodward <jamesrobertwoodward@gmail.com>
2022-05-18 11:01:21 -0500

    add submit, edit, and delete post listeners

--- src/main/java/com/example/restblog/web/PostsController.java
+++ src/main/java/com/example/restblog/web/PostsController.java
@@ -1,35 +1,37 @@
 package com.example.restblog.web;
 
 import com.example.restblog.data.Post;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 
+@CrossOrigin
 @RestController
 @RequestMapping(value="/api/posts", headers = "Accept=application/json")
 public class PostsController {
 
+    List<Post> postList = new ArrayList<>();
+
     @GetMapping
     public List<Post> getAll(){
-        List<Post> postList = new ArrayList<>();
-        Post postOne = new Post(1L,"Cheese is great","Most people love cheese");
-        Post postTwo = new Post(2L,"What is sleep?", "Sleep is the cousin of death");
-        Post postThree = new Post(3L,"Javelin vs. T-72BV", "Javelin wins");
-        postList.add(postOne);
-        postList.add(postTwo);
-        postList.add(postThree);
+        postList.add(new Post(1L,"Cheese is great","Most people love cheese"));
+        postList.add(new Post(2L,"What is sleep?", "Sleep is the cousin of death"));
+        postList.add(new Post(3L,"Javelin vs. T-72BV", "Javelin wins"));
 
         return postList;
     }
 
     @GetMapping("/{id}")
     public Post getById(@RequestParam("id") Long id) {
-        Post testPost = new Post(4L, "What IS a platypus?", "A platypus is a weird venomous duck beaver.");
-
-        return testPost;
+        for (Post post : getAll()) {
+            if(Objects.equals(post.getId(), id)) {
+                return post;
+            }
+        }
+        return new Post();
     }
 
     @PostMapping
@@ -37,4 +39,14 @@ public class PostsController {
         System.out.println(newPost);
     }
 
+    @PutMapping("{id}")
+    public void updatePost(@PathVariable Long id, @RequestBody Post updatedPost) {
+        System.out.println(updatedPost);
+        System.out.println(id);
+    }
+
+    @DeleteMapping("{id}")
+    public void deletePost(@PathVariable Long id) {
+        System.out.println("Deleting post with ID: " + id);
+    }
 }
--- src/main/resources/static/js/views/PostIndex.js
+++ src/main/resources/static/js/views/PostIndex.js
@@ -1,12 +1,121 @@
+import createView from "../createView.js";
+
+const BASE_URL = "http://localhost:8080/api/posts";
+let requestMethod = "POST";
+let postId = "";
+
+
 export default function PostIndex(props) {
+    //language=HTML
     return `
         <header>
             <h1>Posts Page</h1>
         </header>
         <main>
             <div id="posts-container">
-                ${props.posts.map(post => `<h3>${post.title}</h3>`).join('')}   
+                ${props.posts.map(post => `<h3 id="title-${post.id}">${post.title}</h3>
+                                            <p id="content-${post.id}">${post.content}</p>
+<button type="submit" class="btn btn-primary edit-button" data-id="${post.id}">Edit</button>
+<button type="submit" class="btn btn-danger delete-button" data-id="${post.id}">Delete</button>
+`).join('')}
+            </div>
+            <div id="add-post-form">
+                <div>
+                    <input type="text" class="form-control" placeholder="Post Title" id="add-post-title">
+                </div>
+                <br>
+                <div>
+                    <textarea class="form-control" rows="5" type="text" placeholder="Post Content" id="add-post-content"></textarea>
+                </div>
+                <br>
+                <div>
+                    <button type="submit" class="btn btn-primary" id="submit-btn">Submit</button>
+                </div>
             </div>
         </main>
     `;
+}
+
+export function PostsEvent() {
+    createSubmitPostListener();
+    createEditPostListener();
+    createDeletePostListener();
+}
+
+function createSubmitPostListener() {
+    $(document).on('click', '#submit-btn',function (e) {
+        e.preventDefault();
+        const newPost = {
+            title: $("#add-post-title").val(),
+            content: $("#add-post-content").val()
+        }
+
+        const request = {
+            method: requestMethod,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newPost)
+        }
+
+        let requestUrl = "";
+
+        if (postId !== "") {
+            requestUrl = `${BASE_URL}/${postId}`;
+        } else {
+            requestUrl = `${BASE_URL}`;
+        }
+
+        fetch(requestUrl, request)
+            .then(res => {
+                console.log(res.status);
+                // createView("/posts")
+            }).catch(error => {
+            console.log(error);
+            // createView("/posts");
+        }).finally(() => {
+            postId = "";
+            requestMethod = "POST";
+            createView("/posts")
+        })
+    })
+}
+
+function createEditPostListener() {
+    $(document).on('click','.edit-button', function(e) {
+        e.preventDefault();
+        postId = $(this).data('id');
+        requestMethod = "PUT";
+
+        const postTitle = $(`#title-${postId}`).text();
+        const postContent = $(`#content-${postId}`).text();
+
+        $("#add-post-title").val(postTitle);
+        $("#add-post-content").val(postContent);
+
+        console.log(postId);
+        console.log(requestMethod);
+    })
+}
+
+function createDeletePostListener() {
+    $(document).on('click', '.delete-button', function(e) {
+        e.preventDefault();
+
+        const id = $(this).data("id");
+
+        const request = {
+            method: "DELETE",
+        }
+        fetch(`${BASE_URL}/${id}`, request)
+            .then(res => {
+                console.log(res.status);
+                // createView("/posts")
+            }).catch(error => {
+            console.log(error);
+            // createView("/posts");
+        }).finally(() => {
+            createView("/posts")
+        })
+    })
 }

b7bee851abd6ae97ce7c06e844e0a58dc1afe163
James Woodward <jamesrobertwoodward@gmail.com>
2022-05-18 11:04:41 -0500

    add User class

---
+++ src/main/java/com/example/restblog/data/User.java
@@ -0,0 +1,88 @@
+package com.example.restblog.data;
+
+import javax.management.relation.Role;
+import java.util.Date;
+
+public class User {
+
+    Long id;
+    String username;
+    String email;
+    String password;
+    Date createdAt;
+    Role role;
+
+    public enum Role {USER, ADMIN}
+
+    public User(Long id, String username, String email, String password, Date createdAt, Role role) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.createdAt = createdAt;
+        this.role = role;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", createdAt=" + createdAt +
+                ", role=" + role +
+                '}';
+    }
+}

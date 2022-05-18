df1e8ed06cf54a15f97f40b04b0a9266ae0021aa
James Woodward <jamesrobertwoodward@gmail.com>
2022-05-18 11:32:57 -0500

    add getById() method to UsersController

--- src/main/java/com/example/restblog/web/UsersController.java
+++ src/main/java/com/example/restblog/web/UsersController.java
@@ -3,11 +3,7 @@ package com.example.restblog.web;
 import com.example.restblog.data.User;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 import static com.example.restblog.data.User.Role.ADMIN;
 import static com.example.restblog.data.User.Role.USER;
@@ -26,5 +22,15 @@ public class UsersController {
         userList.add(new User(2L, "User2", "user2@email.org", "passwordsareforlosers", new Date(76, Calendar.FEBRUARY, 14), ADMIN));
 
         return userList;
+    }
+
+    @GetMapping("/{id}")
+    public User getById(@RequestParam("id") Long id) {
+        for (User user : getAll()) {
+            if(Objects.equals(user.getId(), id)) {
+                return user;
+            }
+        }
+        return new User();
     }
 }
